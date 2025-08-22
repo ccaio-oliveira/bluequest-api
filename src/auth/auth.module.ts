c061839c5b9d1fcc516ed47e6af6d46b367fa4ce
Caio Oliveira <ccaio.oliveira83@gmail.com>
2025-08-22 14:29:47 -0300

    fix: fix imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,9 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./entities/user.entity";
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from "./auth.service";
+import { AuthController } from "./auth.controller";
+import { JwtStrategy } from "./strategies/jwt.strategy";
+import { RefreshStrategy } from "./strategies/refresh.strategy";
 
 @Module({
     imports: [
