e3c436abded10189b6822309789dc10ddf51b83e
Caio Oliveira <ccaio.oliveira83@gmail.com>
2025-08-22 15:25:50 -0300

    feat: add users repositories

---
+++ src/challenges/challenges.module.ts
@@ -0,0 +1,13 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Challenge } from "./entities/challenge.entity";
+import { User } from "src/auth/entities/user.entity";
+import { ChallengesService } from "./challenges.service";
+import { ChallengesController } from "./challenges.controller";
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Challenge, User])],
+    providers: [ChallengesService],
+    controllers: [ChallengesController],
+})
+export class ChallengeModule {}
