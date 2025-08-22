b24860a5d8f78944a8c566401507f8251aa0025d
Caio Oliveira <ccaio.oliveira83@gmail.com>
2025-08-22 01:01:39 -0300

    feat: add auth dtos

---
+++ src/auth/dtos/register.dto.ts
@@ -0,0 +1,8 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+
+export class RegisterDto {
+    @IsEmail() email!: string;
+    @IsString() @MinLength(6) password!: string;
+    @IsString() @IsNotEmpty() displayName!: string;
+    @IsString() timezone!: string;
+}
